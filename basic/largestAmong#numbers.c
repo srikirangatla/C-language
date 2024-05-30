028d9fb55583b1f28ac7af3a40c8c0efaae79691
Srikiran Gatla <81305709+srikirangatla@users.noreply.github.com>
2024-05-30 06:39:07 +0000

    commit and pushing first time from vscode

---
+++ basic/largestAmong#numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int largestNumber ( int a, int b, int c )
+{
+    int largest;
+
+    if ( a >= b && a >= c )
+        largest = a;
+    else if ( b >= a && b >= c )
+        largest = b;
+    else
+        largest = c;
+    
+    return largest;
+}
+
+int main ()
+{
+    int a, b, c, large;
+
+    printf ("Enter any 3 numbers\n");
+    scanf ("%d %d %d", &a, &b, &c);
+
+    large = largestNumber(a, b, c);
+
+    printf ("Largest Number is %d\n", large);
+
+    return 0;
+}
+
+/*
+output:
+Enter any 3 numbers
+10 20 15
+Largest Number is 20
+*/
